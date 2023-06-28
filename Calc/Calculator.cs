729d02703639fb39dc9cf65279993839db97d55f
karimowakarimova <karimowakarimova@yandex.ru>
2023-06-28 14:48:48 +0500

    new methods

--- Calc/Calculator.cs
+++ Calc/Calculator.cs
@@ -2,60 +2,53 @@
 {
     public class Calculator
     {
-        public int value1 { get; set; }
-        public int value2 { get; set; }
-        public virtual int result { get; set; }
+        public double value1 { get; set; }
+        public double value2 { get; set; }
+        public double result { get; set; }
     }
 
     public class AddCalculator: Calculator
-    {
-        public AddCalculator()
-        {
-            value1 = 0; value2 = 0;
-        }
-        public AddCalculator (int val1, int val2)
+    { 
+        public AddCalculator (double val1 = 0, double val2 = 0)
         {
             value1 = val1; value2 = val2;
+            result = val1 + val2;
         }
-        override public int result => value1 + value2;
     }
     public class SubCalculator : Calculator
-    {
-        public SubCalculator()
-        {
-            value1 = 0; value2 = 0;
-        }
-        public SubCalculator(int val1, int val2)
+    {       
+        public SubCalculator(double val1 = 0, double val2 = 0)
         {
             value1 = val1; value2 = val2;
+            result = val1 - val2;
         }
-        override public int result => value1 - value2;
     }
     public class MultCalculator : Calculator
     {
-        public MultCalculator()
-        {
-            value1 = 0; value2 = 0;
-        }
-        public MultCalculator(int val1, int val2)
+        public MultCalculator(double val1 = 0, double val2 = 0)
         {
             value1 = val1; value2 = val2;
+            result = val1 * val2;
         }
-        override public int result => value1 * value2;
     }
     public class DivCalculator : Calculator
     {
-        public DivCalculator()
-        {
-            value1 = 0; value2 = -1;
-        }
-        public DivCalculator(int val1, int val2)
+        public DivCalculator(double val1 = 0, double val2 = 0)
         {
             value1 = val1;
-            if (val2 == 0)
-            { value1 = 0; value2 = 1; }
-            else value2 = val2;
+            value2 = val2;
+            if (value2 != 0)
+                result = val1 / val2;
+            else result = double.PositiveInfinity;
         }
-        override public int result => value1 / value2;
+    }
+    public class ExCalculator: Calculator
+    {
+        public ExCalculator(double val1 = 0, double val2 = 0)
+        {
+            value1 = val1;
+            value2 = val2;
+            result = Math.Pow(val1, val2);
+        }
     }
 }
--- Calc/Controllers/Calculate.cs
+++ Calc/Controllers/Calculate.cs
@@ -13,15 +13,15 @@ namespace Calc.Controllers
         [HttpGet("Mult")]
         public Calculator GetMult()
         {
-            MultCalculator c = new MultCalculator(Random.Shared.Next(-100, 100), Random.Shared.Next(-100, 100));
+            MultCalculator c = new (Random.Shared.Next(-100, 100), Random.Shared.Next(-100, 100));
             return c;
         }
 
         // POST api/<Calculate>
         [HttpPost("Add")]
-        public string Post(int val1, int val2, int val3)
+        public string Post(double val1, double val2, double val3)
         {
-            AddCalculator c = new AddCalculator(val1, val2);
+            AddCalculator c = new(val1, val2);
             c.value1 = c.result;
             c.value2 = val3;
             return $"{val1} + {val2} + {val3} = {c.result}";
@@ -29,9 +29,9 @@ namespace Calc.Controllers
 
         // PUT api/<Calculate>/5
         [HttpPut("Sub")]
-        public string Put(int val1, int val2, int val3)
+        public string Put(double val1, double val2, double val3)
         {
-            SubCalculator c = new SubCalculator(val1, val2);
+            SubCalculator c = new(val1, val2);
             c.value1 = c.result;
             c.value2 = val3;
             return $"Update {val1} - {val2} - {val3} = {c.result}";
@@ -39,10 +39,30 @@ namespace Calc.Controllers
 
         // DELETE api/<Calculate>/5
         [HttpDelete("Div")]
-        public int Delete(int val1)
+        public double Delete(double val1)
         {
-            DivCalculator c = new DivCalculator(val1, 2);
+            DivCalculator c = new(val1, 2);
             return c.result;
+        }
+
+        [HttpPatch("Exponention")]
+        public string Patch(double val1)
+        {
+            ExCalculator c = new(val1, 2);
+            return $"Update: {c.value1}^{c.value2} = {c.result}";
+        }
+
+        [HttpOptions("Options")]
+        public OkResult Options()
+        {
+            HttpContext.Response.Headers.Add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+            return Ok();
+        }
+
+        [HttpHead]
+        public void Head()
+        {
+            
         }
     }
 }
